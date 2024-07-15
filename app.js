b0151d5717d7e02865c323244fb168d5ea0a8a7b
nodmalikov <abcmalikov@gmail.com>
2024-07-15 15:31:11 +0500

    Initialize commit!

---
+++ app.js
@@ -0,0 +1,44 @@
+const body = document.body;
+const darkMode = localStorage.getItem('darkMode');
+const elDarkToggleButton = document.querySelector('.dark-toggle-button');
+const elFiltersSelect = document.querySelector('.filters-region-select');
+const elFiltersIcon = document.querySelector('.filters-region-icon');
+
+// LOCALSTORAGE
+document.addEventListener('DOMContentLoaded', function() {
+    // SAHIFA YUKLANAYOTGANDA TRANSITION EFFEKTINI OLIB TASHLASH
+    body.classList.add('no-transition');
+
+    if (darkMode === 'enabled') {
+        body.classList.add('dark-mode');
+    }
+
+    // SAHIFA YUKLANGANDAN KEYIN TRANSITION EFFEKTINI QAYTA QO'SHISH
+    setTimeout(() => {
+        body.classList.remove('no-transition');
+    }, 10);
+
+    elDarkToggleButton.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+        if (body.classList.contains('dark-mode')) {
+            localStorage.setItem('darkMode', 'enabled');
+        } else {
+            localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+});
+
+// FOCUS EVENT UCHUN LISTENER
+elFiltersSelect.addEventListener('focus', function () {
+    elFiltersIcon.classList.add('filters-icon-rotate');
+});
+
+// BLUR EVENT UCHUN LISTENER (FOCUS HOLATIDAN CHIQQANDA)
+elFiltersSelect.addEventListener('blur', function () {
+    elFiltersIcon.classList.remove('filters-icon-rotate');
+});
+
+// CHANGE EVENT UCHUN LISTENER (OPTION TANLANGANDA)
+elFiltersSelect.addEventListener('change', function () {
+    elFiltersIcon.classList.remove('filters-icon-rotate');
+});
