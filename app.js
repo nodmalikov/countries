5229af5347bea44567610fc19ce603de7f4c3116
nodmalikov <abcmalikov@gmail.com>
2024-08-12 18:00:14 +0500

    Of site JS code update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const darkMode = localStorage.getItem('darkMode');
 const elDarkToggleButton = document.querySelector('.dark-toggle-button');
 const elFiltersSelect = document.querySelector('.filters-region-select');
 const elFiltersIcon = document.querySelector('.filters-region-icon');
+const elHeader = document.querySelector('.site-header');
+const elMainContent = document.querySelector('.main-content');
 
 // LOCALSTORAGE
 document.addEventListener('DOMContentLoaded', function() {
@@ -10,7 +12,7 @@ document.addEventListener('DOMContentLoaded', function() {
     if (darkMode === 'enabled') {
         body.classList.add('dark-mode');
     }
-
+    
     elDarkToggleButton.addEventListener('click', function() {
         body.classList.toggle('dark-mode');
         if (body.classList.contains('dark-mode')) {
@@ -35,3 +37,25 @@ elFiltersSelect.addEventListener('blur', function () {
 elFiltersSelect.addEventListener('change', function () {
     elFiltersIcon.classList.remove('filters-icon-rotate');
 });
+
+// SCROLL
+window.onscroll = function() {
+    const offset = elHeader.offsetHeight;
+    if (window.scrollY > offset - 20) {
+        elHeader.classList.add('header-border');
+    } else if (window.scrollY < offset - 20) {
+        elHeader.classList.remove('header-border');
+    }
+}
+
+// FUNCTION TO UPDATE MAIN CONTENT'S MARGIN-TOP BASED ON HEADER'S HEIGHT
+function updateMainContentMargin() {
+    const headerHeight = elHeader.offsetHeight;
+    elMainContent.style.marginTop = `${headerHeight}px`;
+}
+
+// UPDATE MAIN CONTENT'S MARGIN INITIALLY
+updateMainContentMargin();
+
+// OPTIONAL: UPDATE MAIN CONTENT'S MARGIN IF THE HEADER'S HEIGHT CHANGES DYNAMICALLY
+new ResizeObserver(updateMainContentMargin).observe(elHeader);
